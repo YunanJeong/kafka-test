1b644a93a6e482551e870137856b591d1d8a498e
yunanjeong <njhgle@gmail.com>
2022-09-26 10:13:30 +0900

    field partition test

--- kafka-control/test_s3_sink_field_partition.py
+++ kafka-control/test_s3_sink_field_partition.py
@@ -1,6 +1,6 @@
 """s3-sink 사용시, AWS Credential 필요.
 
-guava.jar 별도 다운로드 후 플러그인 경로에 설치 필요.
+FieldPartitioner 테스트
 """
 
 import json
@@ -36,24 +36,9 @@ body = {
         "s3.bucket.name": s3_bucket,
         "topics.dir": "yunantest",  # default: topics/
 
-        }
+    }
 }
-# topics: 여러 토픽을 지정가능하다. 리스트가 아니라 따옴표안에 쉼표로 나열해야 한다.
-#"partition.duration.ms"는 새 "path.format"을 생성하는 빈도를 결정하는 수치다.
-# "format.class": "io.confluent.connect.s3.format.parquet.ParquetFormat",
-# "store.kafka.keys": "true",  # default: false. # record의 key를 별도파일로 저장하기. true인데 record의 key가 null이면 에러 발생.
-# "keys.format.class": "io.confluent.connect.s3.format.parquet.ParquetFormat", 별도 저장할 key의 파일 형식
 
-# "parquet.codec": "none",  # ParquetFormat과 세트. s3.compression.type과 역할이 비슷
-# "format.class": "io.confluent.connect.s3.format.json.JsonFormat",
-# "s3.compression.type": "gzip",
-# "timestamp.extractor"
-    # "WallClock": Processing Time. 데이터 파이프라인의 최종앱에서 처리된 시스템 시간.(이 경우 sink connector가 처리한 시간)
-    # "Record": Ingestion Time에 가까움. Record의 Headers(메타데이터)에 있는 timestamp (즉, kafka 토픽에 적재된 시간)
-    # "RecordField": Event Time. 원본로그의 시간. "timestamp.field항목을 추가로 써서 record의 특정 시간 필드를 지정할 수 있음
-    # 각 시간옵션: https://developer.confluent.io/patterns/stream-processing/wallclock-time/
-
-# ensure_ascil 옵션: 한글 등을 아스키가 아니라 한글 그대로 보여줌
 kafka_connect = 'http://'+connect_ip+'/connectors'
 response = requests.post(kafka_connect, json.dumps(body, ensure_ascii=False), headers=headers)
 print(response)
